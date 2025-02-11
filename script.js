f3c09bc80aa606a7e905d30412a4b6c111a765f1
Asmatzaile <gorkainventor@gmail.com>
2025-02-11 12:00:08 +0100

    make bookshelf reflect changes of dialog

--- script.js
+++ script.js
@@ -15,20 +15,35 @@ function addBookToLibrary(title, author, pageCount, isRead) {
 addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, false);
 addBookToLibrary("El perquè de tot plegat", "Quim Monzó", 176, true)
 
-const bookshelf = document.querySelector("#bookshelf");
-myLibrary.forEach(book => {
-    const bookDiv = document.createElement('button');
-    bookDiv.classList.add('book');
-    bookDiv.addEventListener('pointerdown', () => openBookModal(book))
-    bookDiv.textContent = `${book.title} - ${book.author}`;
-    bookshelf.appendChild(bookDiv);
-});
+const drawLibrary = () => {
+    const bookshelf = document.querySelector("#bookshelf");
+    bookshelf.textContent = "";
+    myLibrary.forEach(book => {
+        const bookDiv = document.createElement('button');
+        bookDiv.classList.add('book');
+        bookDiv.addEventListener('pointerdown', () => openBookModal(book))
+        bookDiv.textContent = `${book.title} - ${book.author}`;
+        bookshelf.appendChild(bookDiv);
+    });
+}
+drawLibrary();
+
 
 const bookModal = document.querySelector("#book-modal");
 const openBookModal = (book) => {
-    bookModal.querySelector(".title > [contenteditable]").textContent = book.title;
-    bookModal.querySelector(".author > [contenteditable]").textContent = book.author;
-    bookModal.querySelector(".page-count > [contenteditable]").textContent = book.pageCount;
-    bookModal.querySelector(".is-read").textContent = `${book.isRead ? "Already read" : "Not read yet"}`;
+    const titleField = bookModal.querySelector(".title > [contenteditable]");
+    const authorField = bookModal.querySelector(".author > [contenteditable]");
+    const pageCountField = bookModal.querySelector(".page-count > [contenteditable]");
+    const isReadField = bookModal.querySelector(".is-read");
+    titleField.textContent = book.title;
+    authorField.textContent = book.author;
+    pageCountField.textContent = book.pageCount;
+    isReadField.textContent = `${book.isRead ? "Already read" : "Not read yet"}`;
+    bookModal.querySelector("button").onclick = () => {
+        book.title = titleField.textContent;
+        book.author = authorField.textContent;
+        book.pageCount = pageCountField.textContent;
+        drawLibrary();
+    }
     bookModal.showModal();
 }
